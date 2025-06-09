315e8fc0ac51504c62384bd969fdb9c7d8e511a5
riaanjlagrange <riaanjlagrange@gmail.com>
2025-06-09 04:09:53 +0200

    Highlight text on yank for nvim

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -2,3 +2,10 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+    end,
+})
