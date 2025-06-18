9cf1935410a04eca591ae9e6783e7e01f42a6cbd
riaanjlagrange <riaanjlagrange@gmail.com>
2025-06-18 07:36:58 +0200

    Update nvim obsidian plugin config, update .gitconfig, update zsh and aliases

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -3,6 +3,18 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
+vim.keymap.set("n", "<leader>np", function()
+    vim.cmd("ObsidianToday")
+end, { desc = "Obsidian: Open Daily Note" })
+
+vim.keymap.set("n", "<leader>nw", function()
+    vim.ui.input({ prompt = "New note title: " }, function(input)
+        if input then
+            vim.cmd("ObsidianNew " .. input)
+        end
+    end)
+end, { desc = "Obsidian: New Note" })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
     callback = function()
