c7cd6d4206781450c9e8b74981bb99cb6e00e8ea
riaanjlagrange <riaanjlagrange@gmail.com>
2025-06-21 13:34:37 +0200

    Add obsidian keybinds for yesterday and tomorrow's notes

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -3,9 +3,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
+-- Open yesterday note
+vim.keymap.set("n", "<leader>ny", function()
+    vim.cmd("ObsidianYesterday")
+end, { desc = "Obsidian: Open Yesterday's Daily Note" })
+
+-- Open today's note
 vim.keymap.set("n", "<leader>nd", function()
     vim.cmd("ObsidianToday")
 end, { desc = "Obsidian: Open Daily Note" })
+
+-- Open tomorrow's note
+vim.keymap.set("n", "<leader>nt", function()
+    vim.cmd("ObsidianTomorrow")
+end, { desc = "Obsidian: Open Tomorrow's Daily Note" })
 
 vim.keymap.set("n", "<leader>nn", function()
     vim.ui.input({ prompt = "New note title: " }, function(input)
