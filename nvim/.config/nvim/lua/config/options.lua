e2f316437b210454260cf811a7add5d9d91d0513
riaanjlagrange <riaanjlagrange@gmail.com>
2025-11-08 15:15:54 +0200

    change nvim tab spacing from 4 to 2

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.shell = '/bin/zsh'
 vim.opt.conceallevel = 1
 vim.opt.clipboard = 'unnamedplus'
