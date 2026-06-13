8b0fde6524acdc41d50241b42c29eb601a512c83
riaanjlagrange <riaanjlagrange@gmail.com>
2026-06-13 13:06:23 +0200

    Add barbar & which-key plugins into nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -12,3 +12,13 @@ vim.opt.breakindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.scrolloff = 10
+
+local osc52 = require('vim.ui.clipboard.osc52')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        local regtype = vim.v.event.regtype
+        local regcontents = vim.v.event.regcontents
+        osc52.copy('+')(regcontents, regtype)
+    end,
+})
